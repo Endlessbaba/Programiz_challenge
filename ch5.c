6cf554234373e934652a9ffe1a0b96e65dc3c19a
endlessbaba <energeticokey@gmail.com>
2023-05-13 04:39:47 -0700

    a C program to enter two numbers and find their sum

---
+++ ch5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+
+	
+{
+	int first_number;
+
+	int second_number;
+
+	int sum;
+	
+
+	printf("Input first number: ");
+
+	scanf("%d", &first_number);
+
+	printf("Input second number: ");
+
+	scanf("%d", &second_number);
+
+	sum = first_number + second_number;
+
+
+	printf("sum = %d", sum);
+
+	return (0);
+}
